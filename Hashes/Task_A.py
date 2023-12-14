8a052f37716a2eb46aca72759aebc4b042553960
Mikhail Romanov <87484809+Post-Modern28@users.noreply.github.com>
2023-12-14 22:55:13 +0300

    Create Task_A.py

---
+++ Hashes/Task_A.py
@@ -0,0 +1,33 @@
+# This program reads a string and checks n times whether substrings of length x starting at positions p1 and p2 are the same
+
+modulo = 10**9 + 7
+
+s = input()
+n = len(s)
+x = 257
+h = [0] * (n + 1)
+x_powers = [1] * (n + 1)
+s = ' ' + s
+for i in range(1, n+1):
+    h[i] = (h[i-1] * x + ord(s[i]) - ord('a') + 1) % modulo
+    x_powers[i] = (x_powers[i-1] * x) % modulo
+
+
+def are_same(pos1, pos2, l):
+    if pos1 + l > n + 1 or pos2 + l > n + 1:
+        return False
+    f1 = (h[pos1+l-1] + h[pos2-1] * x_powers[l]) % modulo
+    f2 = (h[pos2+l-1] + h[pos1-1] * x_powers[l]) % modulo
+
+    return f1 == f2
+
+
+k = int(input())
+for k in range(k):
+    length, first, second = map(int, input().split())
+    first += 1
+    second += 1
+    if are_same(first, second, length):
+        print('yes')
+    else:
+        print('no')
